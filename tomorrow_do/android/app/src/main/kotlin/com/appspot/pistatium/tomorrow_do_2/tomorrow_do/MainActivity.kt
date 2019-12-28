5832d4946720779201e73a1e8bdf03bd47e42eba
kimihiro_n <pistatium@users.noreply.github.com>
2019-12-28 19:40:27 +0900

    Setup

---
+++ tomorrow_do/android/app/src/main/kotlin/com/appspot/pistatium/tomorrow_do_2/tomorrow_do/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.appspot.pistatium.tomorrow_do_2.tomorrow_do
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
