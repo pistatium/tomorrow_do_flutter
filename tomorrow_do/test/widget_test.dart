8afc813834deffda744bf9aa6b4fc100e5ea7f41
Kimihiro N <kimihiro.nisimura@gmail.com>
2019-12-30 12:37:27 +0900

    Rebuild package

--- tomorrow_do/test/widget_test.dart
+++ tomorrow_do/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tomorrow_do/main.dart';
+import 'package:tomorrow_do_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
