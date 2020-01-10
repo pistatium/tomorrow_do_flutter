fcb521ebf29d2cfa0e1bfd68ba195a4b267a9e06
Kimihiro N <kimihiro.nisimura@gmail.com>
2020-01-10 09:30:57 +0900

    WIP: tab

--- tomorrow_do/lib/pages/main_page.dart
+++ tomorrow_do/lib/pages/main_page.dart
@@ -10,9 +10,16 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin{
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
   int _counter = 0;
   TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+  }
 
   final List<Tab> tabs = <Tab>[
     Tab(
@@ -42,15 +49,22 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        bottom: TabBar(
+//          isScrollable: true,
+          tabs: tabs,
+          controller: _tabController,
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            TabBarView(
+              controller: _tabController,
+              children: tabs.map((tab) {
+                return _createTab(tab);
+              }).toList(),
             ),
             Text(
               '$_counter',
@@ -59,11 +73,18 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
           ],
         ),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _createTab(Tab tab) {
+    return Center(
+      child: Text("test"),
     );
   }
 }
