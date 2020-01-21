7acc938067bf03b340e6ca1b1f363aff9aa95199
Kimihiro N <kimihiro.nisimura@gmail.com>
2020-01-21 09:35:23 +0900

    WIP: Converter

--- tomorrow_do/lib/converters/todo_converters.dart
+++ tomorrow_do/lib/converters/todo_converters.dart
@@ -1,3 +1,30 @@
+import 'package:tomorrow_do_flutter/entities/todo.dart';
 
+class FirestoreTodoField {
+  static const userId = "user_id";
+  static const title = "title";
+  static const memo = "memo";
+  static const priority = "priority";
+  static const status = "status";
+  static const createdAt = "created_at";
+  static const updatedAt = "updated_at";
+  static const deadlineAt = "deadline_at";
+}
 
-Map
+Map<String, dynamic> todoToFirestoreMap(Todo todo) {
+  return {
+    FirestoreTodoField.userId: todo.userId,
+    FirestoreTodoField.title: todo.title,
+    FirestoreTodoField.memo: todo.memo,
+    FirestoreTodoField.priority: todo.priority,
+    FirestoreTodoField.status: todo.status.toString(),
+    FirestoreTodoField.createdAt: todo.createdAt,
+    FirestoreTodoField.updatedAt: todo.updatedAt,
+    FirestoreTodoField.deadlineAt: todo.deadlineAt,
+  };
+}
+
+Todo todoFromFirestoreMap(Map<String, dynamic> map) {
+  // FIXME
+  return Todo();
+}
