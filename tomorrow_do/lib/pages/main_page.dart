742db09f80433fe78b0e5245d196e0ddbb8d36cf
Kimihiro N <kimihiro.nisimura@gmail.com>
2020-02-07 18:22:49 +0900

    wip

--- tomorrow_do/lib/pages/main_page.dart
+++ tomorrow_do/lib/pages/main_page.dart
@@ -1,25 +1,25 @@
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:tomorrow_do_flutter/converters/todo_converters.dart';
-import 'package:tomorrow_do_flutter/entities/todo.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:tomorrow_do_flutter/converters/todo_converters.dart';
+//import 'package:tomorrow_do_flutter/entities/todo.dart';
 
 final String todoDocumentName = 'todo_test';
 
-class TabData {
-  String label;
-  TodoStatus status;
-
-  TabData({this.label, this.status});
-}
-
-final List<TabData> tabDataList = [
-  TabData(label: "終わった", status: TodoStatus.Done),
-  TabData(label: "今日やる", status: TodoStatus.TodayDo),
-  TabData(label: "明日やる", status: TodoStatus.TomorrowDo),
-  TabData(label: "いつかやる", status:TodoStatus.SomedayDo),
-];
+//class TabData {
+//  String label;
+//  TodoStatus status;
+//
+//  TabData({this.label, this.status});
+//}
+//
+//final List<TabData> tabDataList = [
+//  TabData(label: "終わった", status: TodoStatus.Done),
+//  TabData(label: "今日やる", status: TodoStatus.TodayDo),
+//  TabData(label: "明日やる", status: TodoStatus.TomorrowDo),
+//  TabData(label: "いつかやる", status:TodoStatus.SomedayDo),
+//];
 
 class MainPage extends StatefulWidget {
   MainPage({this.title});
@@ -32,7 +32,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage>
     with SingleTickerProviderStateMixin {
-  TabController _tabController;
+  //TabController _tabController;
 
   @override
   void initState() {
@@ -40,14 +40,14 @@ class _MainPageState extends State<MainPage>
     //_tabController = TabController(length: tabDataList.length, vsync: this);
   }
 
-  void _createTodo() async {
-    final FirebaseAuth _auth = FirebaseAuth.instance;
-    var user = await _auth.currentUser();
-    var todo = Todo.create(user.uid, "test", "", 1, TodoStatus.TodayDo, null);
-    final newDocument =
-        Firestore.instance.collection(todoDocumentName).document();
-    newDocument.setData(todoToFirestoreMap(todo));
-  }
+//  void _createTodo() async {
+//    final FirebaseAuth _auth = FirebaseAuth.instance;
+//    var user = await _auth.currentUser();
+//    var todo = Todo.create(user.uid, "test", "", 1, TodoStatus.TodayDo, null);
+//    final newDocument =
+//        Firestore.instance.collection(todoDocumentName).document();
+//    newDocument.setData(todoToFirestoreMap(todo));
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,33 +75,6 @@ class _MainPageState extends State<MainPage>
 //        tooltip: 'Create',
 //        child: Icon(Icons.add),
 //      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  Widget _createTab(TabData tabData) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance
-          .collection(todoDocumentName)
-          .where(FirestoreTodoField.status, isEqualTo: statusToString(tabData.status))
-          .snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return new Text('Loading...');
-          default:
-            return new ListView(
-              children:
-                  snapshot.data.documents.map((DocumentSnapshot document) {
-                var todo = todoFromFirestoreMap(document.data);
-                return new ListTile(
-                  title: new Text(todo.title),
-                  subtitle: new Text(todo.memo),
-                );
-              }).toList(),
-            );
-        }
-      },
     );
   }
 }
